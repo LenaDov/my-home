568acf368fb12c3d44ab269acc463db96b2537e6
LenaDov <97521808+LenaDov@users.noreply.github.com>
2023-09-07 14:20:07 +0900

    connected joy-ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import '@fontsource/inter'
+
 const root = createRoot(document.getElementById('root')!) // createRoot(container!) if you use TypeScript
 root.render(<App />)
 
