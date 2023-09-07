6c27fd67041ebd95e40067494176fb6e6ea30781
LenaDov <97521808+LenaDov@users.noreply.github.com>
2023-09-07 18:37:33 +0900

    Joy-ui Login component tutorial

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import DateDisplay from '../components/DateDisplay'
 
+import {CssVarsProvider, Sheet, Typography } from '@mui/joy'
+import Login from '../components/Login'
+import CssBaseline from '@mui/joy/CssBaseline/CssBaseline'
+
 const HomePage: React.FC = () => {
     return (
-        <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
-            <DateDisplay />
-        </div>
+        <CssVarsProvider>
+            <Login />
+        </CssVarsProvider>
+
+        // <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        //     <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
+        //     <DateDisplay />
+        // </div>
     )
 }
 
